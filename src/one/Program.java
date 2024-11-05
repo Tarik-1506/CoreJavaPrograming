3fa4de7f3864e473677f8a40ec4a3cff8cc2283f
Tarik <tarikfejzic1506@gmail.com>
2024-11-05 20:42:04 +0100

    ovo je prvi

---
+++ src/one/Program.java
@@ -0,0 +1,12 @@
+package one;
+
+//psvm
+//sout
+// ctrl + alt + l napravi citljivijem
+//ctrl + shift + f10 run
+public class Program {
+    public static void main(String[] args) {
+        System.out.println("hello world");
+        System.out.println("hello world2");
+    }
+}
